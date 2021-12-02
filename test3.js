6dc47e36b35c7d330bfaadad6b2887f742ceb43d
Danyal Isran <91919751+danyal-isran@users.noreply.github.com>
2021-12-02 13:19:30 -0500

    Update test3.js

--- test3.js
+++ test3.js
@@ -1,6 +1,6 @@
-alert(window.ddc);
-console.log('hello');
-
-function test22() {
-  console.log(window);
-}
+(async function initDDCAdapter(WIAPI, dataLayer) {
+  const API = new WIAPI('prodigy-api');
+  const r = await API.utils.getDealerData();
+  console.log(r);
+  console.log(dataLayer.dealership);
+})(window.DDC.API, window.DDC.dataLayer);
